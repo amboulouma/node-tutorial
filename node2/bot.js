cc759ed3e170bbe64d5780707d990acd819dff28
flambok <amine@flambok.com>
2017-12-04 23:48:01 +0100

    Adding config.js contaning API's config

--- node2/bot.js
+++ node2/bot.js
@@ -1,3 +1,9 @@
 console.log('The bot is starting');
 
 var Twit = require('twit');
+
+var config = require('./config');
+
+console.log(config);
+
+var T = new Twit(config);
---
+++ node2/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'KA0aetXMaCFSqrDQjMHNOho8v',
+    consumer_secret:      'iCc9QxULnk76sdGGGLUddwDFlsEzqb8GjAck0szRgYkRTjIbbT',
+    access_token:         '927807627853271040-qPliTBUvQOV5P7jybFA5JkmjT1opvpd',
+    access_token_secret:  '5M2zY8NDumMNaLiaeI4lQtp8b349RSlJ8KDb8IDVZOhA9'
+}
